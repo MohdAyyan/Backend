2b91b44c9f874c08a3b13060d61918c9ba8fdbe1
MohdAyyan <mohdayyan297@gmail.com>
2024-10-12 16:38:38 +0530

    these folder contain all the http method in express using postman

--- 03Url_Shortner/routes/url.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const {handleGenerateCreateURL} = require("../controllers/url")
-const router = express.Router();
-
-router.get("/",handleGenerateCreateURL)
-
-
-module.exports = router;
